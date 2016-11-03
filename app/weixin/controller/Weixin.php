5de22e98f5f7c0f2d59141191b8a67ca5d2498fe
秦智 <631248045@qq.com>
2016-11-03 20:45:27 +0800

    Signed-off-by: 秦智 <631248045@qq.com>

--- app/weixin/controller/Weixin.php
+++ app/weixin/controller/Weixin.php
@@ -82,6 +82,9 @@ class Weixin extends Controller
         }
 
         if(empty($this->member)){
+            Session::delete('openid');
+            Cookie::delete('openid');
+            self::_initialize();
             die('用户不存在');
         }elseif($this->member['status'] == 0){
             die('你的用户已被禁止访问');
